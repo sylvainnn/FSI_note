3778523e0efa0756cc6875cc22b707fc0d4d7e2d
AngelB25 <a.boissie@lyon.ort.asso.fr>
2022-11-21 21:42:43 +0100

    commit

--- public/src/model/DAO/Administrateur_DAO.php
+++ public/src/model/DAO/Administrateur_DAO.php
@@ -2,6 +2,8 @@
 
 namespace DAO;
 
+use DTO\administrateur;
+
 class Administrateur_DAO
 {
     private PDO $bdd;
@@ -14,10 +16,19 @@ class Administrateur_DAO
         $this->bdd = $bdd;
     }
 
-    public function GetAll()
+    public function getAll() : ?array
     {
+        $resultSet = NULL;
+        $req = $this->bdd->query('SELECT * FROM administrateur');
 
+        if ($req) {
+            $req->setFetchMode(\PDO::FETCH_ASSOC);
+            foreach ($req as $row) {
+                $resultSet[] = new administrateur($row);
+
+            }
+        }
+        return $resultSet;
     }
-
 
 }
--- public/src/model/DAO/Entreprise_DAO.php
+++ public/src/model/DAO/Entreprise_DAO.php
@@ -2,7 +2,32 @@
 
 namespace DAO;
 
+use DTO\Entreprise;
+
 class Entreprise_DAO
 {
+    private PDO $bdd;
 
+    /**
+     * @param PDO $bdd
+     */
+    public function __construct(PDO $bdd)
+    {
+        $this->bdd = $bdd;
+    }
+
+    public function getAll() : ?array
+    {
+        $resultSet = NULL;
+        $req = $this->bdd->query('SELECT * FROM Entreprise');
+
+        if ($req) {
+            $req->setFetchMode(\PDO::FETCH_ASSOC);
+            foreach ($req as $row) {
+                $resultSet[] = new Entreprise($row);
+
+            }
+        }
+        return $resultSet;
+    }
 }
