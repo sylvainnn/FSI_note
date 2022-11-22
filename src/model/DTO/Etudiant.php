2ca48518d7e8d11ade1811b7e537722e3094bc12
Ezeit <46382608+xEzeit@users.noreply.github.com>
2022-11-22 16:54:56 +0100

    New tree structure.

--- src/model/DTO/Etudiant.php
+++ src/model/DTO/Etudiant.php
@@ -8,9 +8,12 @@ class Etudiant extends Utilisateur
     private int $id_etu;
     private string $classe_etu;
     private string $adresse_etu;
-    private ?Entreprise $entreprise_etu = null;
-    private ?Tuteur $tuteur_etu = null;
+    private Entreprise $entreprise_etu;
+    private Tuteur $tuteur_etu;
     private array $mesNotes_etu;
+
+    //HERITAGE
+    public string $nom;
 
     /**
      * @param int $id_etu
@@ -29,101 +32,4 @@ class Etudiant extends Utilisateur
         $this->tuteur_etu = $data["tuteur_etu"];
         $this->mesNotes_etu = $data["mesNotes_etu"];
     }
-
-    /**
-     * @return int|mixed
-     */
-    public function getIdEtu(): mixed
-    {
-        return $this->id_etu;
-    }
-
-    /**
-     * @param int|mixed $id_etu
-     */
-    public function setIdEtu(mixed $id_etu): void
-    {
-        $this->id_etu = $id_etu;
-    }
-
-    /**
-     * @return mixed|string
-     */
-    public function getClasseEtu(): mixed
-    {
-        return $this->classe_etu;
-    }
-
-    /**
-     * @param mixed|string $classe_etu
-     */
-    public function setClasseEtu(mixed $classe_etu): void
-    {
-        $this->classe_etu = $classe_etu;
-    }
-
-    /**
-     * @return mixed|string
-     */
-    public function getAdresseEtu(): mixed
-    {
-        return $this->adresse_etu;
-    }
-
-    /**
-     * @param mixed|string $adresse_etu
-     */
-    public function setAdresseEtu(mixed $adresse_etu): void
-    {
-        $this->adresse_etu = $adresse_etu;
-    }
-
-    /**
-     * @return Entreprise|mixed
-     */
-    public function getEntrepriseEtu(): mixed
-    {
-        return $this->entreprise_etu;
-    }
-
-    /**
-     * @param Entreprise|mixed $entreprise_etu
-     */
-    public function setEntrepriseEtu(mixed $entreprise_etu): void
-    {
-        $this->entreprise_etu = $entreprise_etu;
-    }
-
-    /**
-     * @return Tuteur|mixed
-     */
-    public function getTuteurEtu(): mixed
-    {
-        return $this->tuteur_etu;
-    }
-
-    /**
-     * @param Tuteur|mixed $tuteur_etu
-     */
-    public function setTuteurEtu(mixed $tuteur_etu): void
-    {
-        $this->tuteur_etu = $tuteur_etu;
-    }
-
-    /**
-     * @return array|mixed
-     */
-    public function getMesNotesEtu(): mixed
-    {
-        return $this->mesNotes_etu;
-    }
-
-    /**
-     * @param array|mixed $mesNotes_etu
-     */
-    public function setMesNotesEtu(mixed $mesNotes_etu): void
-    {
-        $this->mesNotes_etu = $mesNotes_etu;
-    }
-
 }
