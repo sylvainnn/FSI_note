c899965f25ec9c275cba1e1203cc92b8d9b16565
sylvain <s.crochon@lyon.ort.asso.fr>
2022-11-24 15:27:22 +0100

    css

---
+++ src/view/connexion.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <!-- importer le fichier de style -->
+    <link rel="stylesheet" href="../../public/css/css.css" media="screen" type="text/css"/>
+
+</head>
+<body>
+<div class="sectionLeft"><h1>Connexion</h1>
+    <br><br><br><br><br><br><br><br>
+    <span style="position: absolute; top: 110px; left: 90px;">
+        <img src="../../public/css/images/sitting-4.png" height="170px">
+    </span>
+    <br><br><br><br><b><h3>Gestion du tutorat</h3></b>
+    <br>ORT Etudes Supérieures
+    <br><br>
+    <img src="../../public/css/images/Slide%20Lines.png" height="4px">
+</div>
+<div class="sectionRight">Test</div>
+</body>
+</html>
