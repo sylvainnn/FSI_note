e836be48cb08230a979d04cb0b704ccab4f8296d
AngelB25 <a.boissie@lyon.ort.asso.fr>
2022-11-24 16:58:06 +0100

    commit

--- src/model/DAO/Administrateur_DAO.php
+++ src/model/DAO/Administrateur_DAO.php
@@ -31,4 +31,60 @@ class Administrateur_DAO
         return $resultSet;
     }
 
+    public function GetById{
+        $resultSet = NULL;
+        $query = 'SELECT * FROM administrateur WHERE id_ad=:id_ad;';
+
+        // On prépare la rêquete
+        $reqPrep = $this->bdd->prepare($query);
+
+        $res = $reqPrep->execute([':id_ad' => $id]);
+
+        if ($res !== FALSE) {
+            $tab = ($tmp = $reqPrep->fetch(\PDO::FETCH_ASSOC)) ? $tmp : null;
+            if(!is_null($tab)) {
+                // Si on récupère une occurence, on crée un objet PERSONNEL avec cette dernière
+                $resultSet = new administrateur($tab);
+            }
+        }
+        return $resultSet;
+    }
+
+    public function authentify(Administrateur $entity): bool{
+        $resultSet = NULL;
+        if ($entity != NULL) {
+            if ($this->bdd) {
+                //  Conversion de l'entité en tableau associatif (nécessaire pour le binding)
+                $query = 'SELECT * FROM Administrateur WHERE logAdmin = :login';
+                //  Préparation et exécution de la requête
+                $reqPrep = $this->bdd->prepare($query);
+                $reqPrep->bindValue(':login', $entity->getLoginPersonnel());
+                $rqtResult = $reqPrep->execute();
+                if ($rqtResult) {
+                    //  Récupération des résultats d'exécution de la requête
+                    $row = $reqPrep->fetch(\PDO::FETCH_ASSOC);
+                    if ($row) {
+                        //  On a un résultat
+                        $administrateur = new administrateur($row);
+
+                        if (password_verify($entity->getMdpAd(), $administrateur->getMdpAd())) {
+                            // if ($entity->getMdpPersonnel()== $personnel->getMdpPersonnel()){
+                            //  On ne conserve nulle part le password en dehors de la bdd...
+                            $administrateur->setMdpAd('');
+                            //  Mise en place des variables de session
+                            // A FIARE PLUS TARD DANS LE SUJET
+                            $_SESSION['Administrateur'] = $administrateur;
+                            //  Authentification réussie, on retournera TRUE
+                            $resultSet = TRUE;
+                        } else
+                            $resultSet = FALSE;
+                    } else
+                        $resultSet = FALSE;
+                } else
+                    $resultSet = FALSE;
+            } else
+                $resultSet = FALSE;
+        }
+        return $resultSet;
+    }
 }
