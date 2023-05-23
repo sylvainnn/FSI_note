756a1d9356a069fc2667e2630c36f4104e675154
florian NEEMA <f.neema@lyon.ort.asso.fr>
2023-05-23 15:59:37 +0200

    der version fsi

--- config/appConfig.php
+++ config/appConfig.php
@@ -11,12 +11,12 @@ if(!defined('DUMP')) {
 $Bdd = array(
     'interface' => 'pdo',
     'type'   => 'mysql',	    //  mysql ou pgsql
-    'host'   => 'localhost',     // l'emplacement de votre site, dans le cas d'un site en local => localhost
+    'host'   => 'db5013128370.hosting-data.io',     // l'emplacement de votre site, dans le cas d'un site en local => localhost
     'port'   =>  '3306',	    // Par défaut: 5432 pour postgreSQL, 3306 pour MySQL
     'charset' => 'UTF8',
-    'dbname' => 'p2023_2sio_projet_tutorat', // nom de votre BDD
-    'username'   => 'root', // Utilisateur de connexion à votre BDD, sous MySQL par défaut c'est root
-    'password'   => '', // mot de passe pour se connecter à votre BDD, sous mySQL par défaut c'est vide
+    'dbname' => 'dbs11018590', // nom de votre BDD
+    'username'   => 'dbu5502390', // Utilisateur de connexion à votre BDD, sous MySQL par défaut c'est root
+    'password'   => 'JY4EFYsxBTnWL9N4', // mot de passe pour se connecter à votre BDD, sous mySQL par défaut c'est vide
 );
 
 require_once ('globalConfig.php');
